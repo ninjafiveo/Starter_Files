44397b8c8de26fa4adaa9f7d57aeb86815861d02
ninjafiveo <mjsekol@live.com>
2023-10-05 09:16:52 -0400

    first commit

---
+++ The_Galactic_Database_Project_Starter.py
@@ -0,0 +1,38 @@
+# FOR EACH SECTION BELOW, YOU'LL NEED TO DELETE THE WORD "pass" TO BEGIN WORKING ON THAT SECTION OF THE CODE. "pass" IS ONLY THERE SO THAT BLOCK OF CODE DOESN'T PRODUCE AN ERROR WHILE YOU ARE WRITING THE CODE.
+class Clone:
+    def __init__(self, name, rank, planet):
+        pass
+        # TODO: Initialize the attributes for the Clone
+        
+    # TODO: Create a method to display the Clone's info in a readable format
+
+
+class Starship:
+    def __init__(self, name, model, capacity):
+        pass
+        # TODO: Initialize the attributes for the Starship
+    
+    # TODO: Create a method to display the Starship's info in a readable format
+
+
+class GalacticDatabase:
+    def __init__(self):
+        pass
+        # TODO: Initialize empty lists to store Clone and Starship objects
+
+    def add_clone(self, name, rank, planet):
+        # TODO: Create a new Clone object and add it to the respective list
+        pass
+    def add_starship(self, name, model, capacity):
+        
+        # TODO: Create a new Starship object and add it to the respective list
+        pass
+    def view_clones(self):
+        # TODO: Display information for all the clones stored in the database
+        pass
+    def view_starships(self):
+        # TODO: Display information for all the starships stored in the database
+        pass
+    # TODO: Additional methods to retrieve and display specific clone/starship details based on user input
+    
+# TODO: Implement user interaction to utilize the functionalities in the GalacticDatabase class
